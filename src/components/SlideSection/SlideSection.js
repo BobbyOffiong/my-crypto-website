e4d9126afac3ebf48b209478b09f8fedc5fa8972
Bobby Offiong <89223802+BobbyOffiong@users.noreply.github.com>
2025-03-02 17:07:21 +0100

    Update SlideSection.js

--- src/components/SlideSection/SlideSection.js
+++ src/components/SlideSection/SlideSection.js
@@ -64,13 +64,13 @@ export default function SlideSection() {
                 className="h-full w-full object-cover object-center" 
               />
               {/* White Background Overlay Inside Image */}
-              <div className="absolute inset-0 bg-white opacity-50"></div>
+              <div className="absolute inset-0 bg-white opacity-40"></div>
             </div>
 
             {/* Dark Texts Over Image */}
             <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-6 z-10">
-              <h1 className="text-4xl font-bold text-orange-400">{slide.title}</h1>
-              <p className="mt-2 text-sm text-gray-800 font-bold">{slide.subtitle}</p>
+              <h1 className="text-4xl font-bold text-orange-300">{slide.title}</h1>
+              <p className="mt-2 text-sm text-gray-700 font-bold">{slide.subtitle}</p>
               <button
                 onMouseDown={(e) => e.currentTarget.classList.add("bg-orange-300")}
                 onMouseUp={(e) => e.currentTarget.classList.remove("bg-orange-300")}
