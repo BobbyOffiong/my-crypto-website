56bfa086336e53d1bc4288991adc7275891b29fa
Bobby Offiong <89223802+BobbyOffiong@users.noreply.github.com>
2025-03-02 10:44:05 +0100

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -71,7 +71,7 @@ const Header = () => {
         </div>
 
         {/* Navigation Menu */}
-        <nav className="space-x-6 md:hidden lg:block">
+        <nav className="space-x-6 lg:block">
           <Link to="/" className="hover:text-orange-200">{polyglot.t("home")}</Link>
           <Link to="/about" className="hover:text-orange-200">{polyglot.t("about")}</Link>
           <Link to="/services" className="hover:text-orange-200">{polyglot.t("services")}</Link>
