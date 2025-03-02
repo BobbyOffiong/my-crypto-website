64f8908df3d47d863353a3284bc0ff2b4380f9f9
Bobby Offiong <89223802+BobbyOffiong@users.noreply.github.com>
2025-03-02 10:24:49 +0100

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -66,7 +66,7 @@ const Header = () => {
         </div>
 
         {/* Cryptocurrency Prices - Visible on large screens */}
-        <div className="crypto-prices block md:block">
+        <div className="crypto-prices block">
           <CryptoPrices />
         </div>
 
