2e8137affc3c95d4ee198faa8ccd6c1ed62ec45f
Bobby Offiong <89223802+BobbyOffiong@users.noreply.github.com>
2026-04-27 20:38:48 +0100

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import AboutPage from "../components/AboutPage/AboutPage"
+import MovingCoins from "../components/MovingCoins/MovingCoins";
+import GetStarted from "../components/GetStarted/GetStarted";
+import WhyChooseUs from "../components/WhyChooseUs/WhyChooseUs";
+import FutureOfDM from "../components/FutureOfDM/FutureOfDM";
+import Partnership from "../components/Patnership/Patnership";
+import Calculator from "../components/Calculator/Calculator";
+import Newsletter from "../components/Newsletter/Newsletter";
+import Footer from "../components/Footer/Footer";
+
+const About = () => {
+    return (
+        <>
+        <AboutPage />
+        <MovingCoins />
+        <GetStarted />
+        <WhyChooseUs />
+        <FutureOfDM />
+        <Partnership />
+        <Calculator />
+        <Newsletter />
+        <Footer />
+        </>
+    );
+  };
+  
+  export default About;
---
+++ src/pages/Services.js
@@ -0,0 +1,27 @@
+import ServicesPage from "../components/ServicesPage/ServicesPage"
+import MovingCoins from "../components/MovingCoins/MovingCoins";
+import GetStarted from "../components/GetStarted/GetStarted";
+import WhyChooseUs from "../components/WhyChooseUs/WhyChooseUs";
+import FutureOfDM from "../components/FutureOfDM/FutureOfDM";
+import Partnership from "../components/Patnership/Patnership";
+import Calculator from "../components/Calculator/Calculator";
+import Newsletter from "../components/Newsletter/Newsletter";
+import Footer from "../components/Footer/Footer";
+
+const Services = () => {
+    return (
+        <>
+        <ServicesPage />
+        <MovingCoins />
+        <GetStarted />
+        <WhyChooseUs />
+        <FutureOfDM />
+        <Partnership />
+        <Calculator />
+        <Newsletter />
+        <Footer />
+        </>
+    );
+  };
+  
+  export default Services;
